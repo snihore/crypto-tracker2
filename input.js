3ca6500bc67f53c426f9e63a10e575b92c1fc359
snihore <souravnihore1998@gmail.com>
2021-05-04 18:10:59 +0530

    test1

--- input.js
+++ input.js
@@ -4,29 +4,29 @@ const inputData = [
         quantity: 7,
         name: "Devesh Muradiya",
         type:"DOGE/INR",
-        smallType:"dogeinr"
-    },
-    {
-        buyAt: 18.029,
-        quantity: 5,
-        name: "Sourabh Nihore",
-        type:"DOGE/INR",
-        smallType:"dogeinr"
-    },
-    {
-        buyAt: 0.55745,
-        quantity: 225,
-        name: "Devesh Muradiya",
-        type:"BTT/INR",
-        smallType:"bttinr"
-    },
-    {
-        buyAt: 4510000,
-        quantity: 0.00002,
-        name: "Devesh Muradiya",
-        type:"BTC/INR",
-        smallType:"btcinr"
-    }
+        smallType:"dogeinr"}
+    // },
+    // {
+    //     buyAt: 18.029,
+    //     quantity: 5,
+    //     name: "Sourabh Nihore",
+    //     type:"DOGE/INR",
+    //     smallType:"dogeinr"
+    // },
+    // {
+    //     buyAt: 0.55745,
+    //     quantity: 225,
+    //     name: "Devesh Muradiya",
+    //     type:"BTT/INR",
+    //     smallType:"bttinr"
+    // },
+    // {
+    //     buyAt: 4510000,
+    //     quantity: 0.00002,
+    //     name: "Devesh Muradiya",
+    //     type:"BTC/INR",
+    //     smallType:"btcinr"
+    // }
 ];
 
 
